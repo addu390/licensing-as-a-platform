f0ef8b2166ebb1304b45f67fd890fbc41f62b059
Adesh Nalpet <390.adesh@gmail.com>
2020-10-08 00:50:40 +0530

    Include model constants

--- core/constants.py
+++ core/constants.py
@@ -6,6 +6,12 @@ EXPIRED = "EXPIRED"
 
 IN_ACTIVE = "IN_ACTIVE"
 
+STATUS = "status"
+PACKAGE = "package"
+ACTIVATION_DATE = "activation_date"
+EXPIRY_DATE = "expiry_date"
+LICENSE = "license"
+
 LICENSE_STATUS = (
     ("TRIAL", 'Trial License'),
     ("IN_PROGRESS", 'License activation in progress'),
