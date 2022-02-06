c4b9f28f2ded95b1455e02ce056f3d9c24beb1f5
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-02-06 02:28:18 -0400

    Move redis location to Configuration

--- licensing_platform/settings.py
+++ licensing_platform/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/",
+        "LOCATION": config('REDIS_LOCATION'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         }
