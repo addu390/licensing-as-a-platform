10e0b79c8ffa37dac8331f51c7e1a3d7a67d756e
Adesh Nalpet Adimurthy <390.adesh@gmail.com>
2022-02-07 04:10:28 -0400

    Update deploy.sh start script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+source sandbox/bin/activate
+cd licensing-as-a-platform/
+sudo git pull origin master
+sudo python -m pip install -r requirements.txt
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic
+sudo systemctl restart nginx
+sudo systemctl restart gunicorn
