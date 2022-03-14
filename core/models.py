58468b02d90f25e139e23ecc03e354b330de53b9
Sherrin Raji <sh810841@dal.ca>
2022-03-14 19:43:42 -0300

    format model.py

--- core/models.py
+++ core/models.py
@@ -11,23 +11,25 @@ def new_uuid():
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    external_id = models.CharField(max_length=255, unique=True, default=new_uuid)
+    external_id = models.CharField(
+        max_length=255, unique=True, default=new_uuid)
 
     class Meta:
         abstract = True
 
 
 class License(BaseModel):
-    license_id = models.CharField(max_length=255, unique=True, default=new_uuid)
+    license_id = models.CharField(
+        max_length=255, unique=True, default=new_uuid)
     linked_to = models.CharField(max_length=255, null=True, blank=True)
-    status = models.CharField(choices=LICENSE_STATUS, default=ACTIVE, max_length=100)
+    status = models.CharField(choices=LICENSE_STATUS,
+                              default=ACTIVE, max_length=100)
     activation_date = models.DateTimeField(null=True, blank=True)
     expiry_date = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         db_table = LICENSE
         verbose_name_plural = LICENSE
-
 
     def __str__(self):
         return self.license_id
@@ -39,7 +41,8 @@ class Plan(BaseModel):
     image = models.FileField(blank=True, null=True)
     type = models.CharField(choices=PLAN_TYPE, default=REGULAR, max_length=50)
     price = models.IntegerField(default=0)
-    status = models.CharField(choices=BASE_STATUS, default=ACTIVE, max_length=100)
+    status = models.CharField(
+        choices=BASE_STATUS, default=ACTIVE, max_length=100)
 
     class Meta:
         db_table = PLAN
@@ -54,7 +57,8 @@ class Feature(BaseModel):
     description = models.TextField(null=True)
     image = models.FileField(blank=True, null=True)
     type = models.CharField(choices=FEATURE_TYPE, default=LIMIT, max_length=50)
-    status = models.CharField(choices=BASE_STATUS, default=ACTIVE, max_length=100)
+    status = models.CharField(
+        choices=BASE_STATUS, default=ACTIVE, max_length=100)
 
     class Meta:
         db_table = FEATURE
@@ -65,42 +69,46 @@ class Feature(BaseModel):
 
 
 class LicensePlan(BaseModel):
-    license = models.ForeignKey(License, to_field='license_id', on_delete=models.DO_NOTHING)
-    plan = models.ForeignKey(Plan, to_field='external_id', on_delete=models.DO_NOTHING)
+    license = models.ForeignKey(
+        License, to_field='license_id', on_delete=models.DO_NOTHING)
+    plan = models.ForeignKey(
+        Plan, to_field='external_id', on_delete=models.DO_NOTHING)
 
     class Meta:
         db_table = "license_plan"
         verbose_name_plural = "License and Plan"
 
-
     def __str__(self):
-        return "%s -> %s" % (str(self.license), str(self.plan))    
+        return "%s -> %s" % (str(self.license), str(self.plan))
 
 
 class FeaturePlan(BaseModel):
-    feature = models.ForeignKey(Feature, to_field='external_id', related_name="feature_plan", on_delete=models.DO_NOTHING)
-    plan = models.ForeignKey(Plan, to_field='external_id', related_name="selected_plan", on_delete=models.DO_NOTHING)
+    feature = models.ForeignKey(Feature, to_field='external_id',
+                                related_name="feature_plan", on_delete=models.DO_NOTHING)
+    plan = models.ForeignKey(Plan, to_field='external_id',
+                             related_name="selected_plan", on_delete=models.DO_NOTHING)
 
     class Meta:
         db_table = "feature_plan"
         verbose_name_plural = "Feature and Plan"
-
 
     def __str__(self):
         return "%s -> %s" % (str(self.plan), str(self.feature))
 
 
 class LicenseFeature(BaseModel):
-    license = models.ForeignKey(License, to_field='license_id', on_delete=models.DO_NOTHING)
-    plan = models.ForeignKey(Plan, to_field='external_id', on_delete=models.DO_NOTHING)
-    feature = models.ForeignKey(Feature, to_field='external_id', on_delete=models.DO_NOTHING)
+    license = models.ForeignKey(
+        License, to_field='license_id', on_delete=models.DO_NOTHING)
+    plan = models.ForeignKey(
+        Plan, to_field='external_id', on_delete=models.DO_NOTHING)
+    feature = models.ForeignKey(
+        Feature, to_field='external_id', on_delete=models.DO_NOTHING)
     usage = models.IntegerField(default=0)
     threshold = models.IntegerField(default=0)
 
     class Meta:
         db_table = "license_feature"
         verbose_name_plural = "License and Feature"
-
 
     def __str__(self):
         return "%s -> %s -> %s" % (str(self.license), str(self.plan), str(self.feature))
